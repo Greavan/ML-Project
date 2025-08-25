87289b1a002ddd04947b0b2383c8c7ddb3e67c5f
Greavan <greavan7@gmail.com>
2025-08-25 18:12:10 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,42 @@
+import sys
+import logging
+import os
+from datetime import datetime
+
+# ✅ Setup logging to file
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occurred in python script [{0}] line number [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+
+        # ✅ Automatically log the error
+        logging.error(self.error_message)
+
+    def __str__(self):
+        return self.error_message
+
+# Example usage
+if __name__ == "__main__":
+    try:
+        a = 1 / 0   # Will raise ZeroDivisionError
+    except Exception as e:
+        raise CustomException(e, sys)
